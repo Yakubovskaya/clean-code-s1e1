17407374c8d7a763747f7a97e5231675b24a25bd
Marina Yakubovskaya <maribars93@yandex.ru>
2025-10-20 22:49:32 +0300

    style(no-rule): improve code readability with proper line breaks and spacing in js file

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const taskInput=document.getElementById("new-task");
-const addButton=document.querySelector(".todo__add-btn");
-const incompleteTaskHolder=document.querySelector(".todo__list_incomplete");
-const completedTasksHolder=document.querySelector(".todo__list_complete");
+const taskInput = document.getElementById("new-task");
+const addButton = document.querySelector(".todo__add-btn");
+const incompleteTaskHolder = document.querySelector(".todo__list_incomplete");
+const completedTasksHolder = document.querySelector(".todo__list_complete");
 
 
 
-const createNewTaskElement=function(taskString){
+const createNewTaskElement = function(taskString) {
 
-    const listItem=document.createElement("li");
+    const listItem = document.createElement("li");
     listItem.className = "todo__item";
   
-    const checkBox=document.createElement("input");
+    const checkBox = document.createElement("input");
     checkBox.type = "checkbox";
     checkBox.className = "todo__checkbox";
    
-    const label=document.createElement("label");
+    const label = document.createElement("label");
     label.className = "todo__task";
     label.innerText = taskString;
     
-    const editInput=document.createElement("input");
+    const editInput = document.createElement("input");
     editInput.type = "text";
     editInput.className = "todo__task-input";
     editInput.value = taskString;
    
-    const editButton=document.createElement("button");
+    const editButton = document.createElement("button");
     editButton.className = "button todo__edit-btn";
     editButton.innerText = "Edit";
    
-    const deleteButton=document.createElement("button");
+    const deleteButton = document.createElement("button");
     deleteButton.className = "button todo__delete-btn";
-    const deleteButtonImg=document.createElement("img");
+    const deleteButtonImg = document.createElement("img");
     deleteButtonImg.className = "todo__delete-icon";
     deleteButtonImg.src = './remove.svg';
     deleteButtonImg.alt = "Delete";
@@ -47,10 +47,10 @@ const createNewTaskElement=function(taskString){
 
 
 
-const addTask=function(){
+const addTask = function() {
     console.log("Add Task...");
     if (!taskInput.value) return;
-    const listItem=createNewTaskElement(taskInput.value);
+    const listItem = createNewTaskElement(taskInput.value);
 
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskCompleted);
@@ -60,25 +60,25 @@ const addTask=function(){
 
 
 
-const editTask=function(){
+const editTask = function() {
     console.log("Edit Task...");
     console.log("Change 'edit' to 'save'");
 
-    const listItem=this.parentNode;
+    const listItem = this.parentNode;
 
-    const editInput=listItem.querySelector('.todo__task-input');
-    const label=listItem.querySelector(".todo__task");
-    const editBtn=listItem.querySelector(".todo__edit-btn");
-    const containsClass=listItem.classList.contains("todo__item_edit-mode");
+    const editInput = listItem.querySelector('.todo__task-input');
+    const label = listItem.querySelector(".todo__task");
+    const editBtn = listItem.querySelector(".todo__edit-btn");
+    const containsClass = listItem.classList.contains("todo__item_edit-mode");
  
-    if(containsClass){
+    if (containsClass) {
     	label.innerText = editInput.value;
     	label.classList.remove("todo__task_edit-mode");
     	editInput.classList.remove("todo__task-input_edit-mode");
     	editInput.style.display = "none";
     	label.style.display = "block";
     	editBtn.innerText = "Edit";
-    }else{
+    } else {
         editInput.value = label.innerText;
         label.classList.add("todo__task_edit-mode");
         editInput.classList.add("todo__task-input_edit-mode");
@@ -89,23 +89,23 @@ const editTask=function(){
     listItem.classList.toggle("todo__item_edit-mode");
 };
 
-const deleteTask=function(){
+const deleteTask = function() {
     console.log("Delete Task...");
 
-    const listItem=this.parentNode;
-    const ul=listItem.parentNode;
+    const listItem = this.parentNode;
+    const ul = listItem.parentNode;
 
     ul.removeChild(listItem);
 }
 
 
 
-const taskCompleted=function(){
+const taskCompleted = function() {
     console.log("Complete Task...");
 
-    const listItem=this.parentNode;
+    const listItem = this.parentNode;
 
-	const label=listItem.querySelector(".todo__task");
+	const label = listItem.querySelector(".todo__task");
 
 	listItem.classList.add("todo__item_done");
 	label.classList.add("todo__task_line-through");
@@ -116,48 +116,48 @@ const taskCompleted=function(){
 }
 
 
-const taskIncomplete=function(){
+const taskIncomplete = function() {
     console.log("Incomplete Task...");
 
-    const listItem=this.parentNode;
-	const label=listItem.querySelector(".todo__task");
+    const listItem = this.parentNode;
+	const label = listItem.querySelector(".todo__task");
 
 	label.classList.remove("todo__task_line-through");
 
     incompleteTaskHolder.appendChild(listItem);
-    bindTaskEvents(listItem,taskCompleted);
+    bindTaskEvents(listItem, taskCompleted);
 }
 
 
 
-const ajaxRequest=function(){
+const ajaxRequest = function() {
     console.log("AJAX Request");
 }
 
-addButton.addEventListener("click",addTask);
-addButton.addEventListener("click",ajaxRequest);
+addButton.addEventListener("click", addTask);
+addButton.addEventListener("click", ajaxRequest);
 
 
-const bindTaskEvents=function(taskListItem,checkBoxEventHandler){
+const bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
     console.log("bind list item events");
 
-    const checkBox=taskListItem.querySelector(".todo__checkbox");
-    const editButton=taskListItem.querySelector(".todo__edit-btn");
-    const deleteButton=taskListItem.querySelector(".todo__delete-btn");
+    const checkBox = taskListItem.querySelector(".todo__checkbox");
+    const editButton = taskListItem.querySelector(".todo__edit-btn");
+    const deleteButton = taskListItem.querySelector(".todo__delete-btn");
 
-    editButton.onclick=editTask;
+    editButton.onclick = editTask;
 
-    deleteButton.onclick=deleteTask;
+    deleteButton.onclick = deleteTask;
 
-    checkBox.onchange=checkBoxEventHandler;
+    checkBox.onchange = checkBoxEventHandler;
 }
 
 
-for (let i=0; i<incompleteTaskHolder.children.length;i++){
-    bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
+for (let i = 0; i < incompleteTaskHolder.children.length; i++) {
+    bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
 }
 
 
-for (let i=0; i<completedTasksHolder.children.length;i++){
-    bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
+for (let i = 0; i < completedTasksHolder.children.length; i++){
+    bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
